8cb168cd977b1dadaca5e1fe7fa28e886eb809e3
splean101 <40311839+splean101@users.noreply.github.com>
2018-12-04 08:04:47 +0200

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,23 +1,26 @@
 'use strict';
 
-Создайте тип объектов Прямоугольник со свойствами координат верхнего левого (x1, y1) и нижнего правого (x2, y2) углов. 
-Переопределите методы toString() и valueOf(). Метод toString() должен возвращать строку с описанием текущего состояния.
- Метод valueOf() должен возвращать текущий периметр.*/
- function Rectangle(x1, y1, x2, y2){
-	this.x1 = x1;
-	this.y1 = y1;
-	this.x2 = x2;
-	this.y2 = y2;
+var arr = [];
+for (var i = 0; i >= 0; i++){
+	var temp = prompt('put your string', '');
+	if( temp !== 'end'){
+		arr[i] = temp;
+	}else{
+		break
+		};
 };
-Rectangle.prototype.toString = function(){
-	alert('this x1 = '+ this.x1);
-	alert('this y1 = '+ this.y1);
-};
-Rectangle.prototype.valueOf = function(){
-	alert('rectangle perimeter = '+ this.x1);
-};
+console.log(arr);
 
-var r = new Rectangle(1,3,5,7);
-r.toString();
-console.log(Rectangle.toString);
-console.log(r.toString);
+var arr1 = arr;
+for (var key in arr1){
+	if (isNaN( arr1[key])){
+		arr1[key] = 'No numbers: ' + arr1[key];
+	}else{
+		arr1[key] = 'Numbers: ' + arr1[key];
+	};
+};
+console.log(arr1);
+
+for (var j = 0; j < arr.length; j++){
+	console.log(arr[j] + ' ' + arr1[j]);
+};
